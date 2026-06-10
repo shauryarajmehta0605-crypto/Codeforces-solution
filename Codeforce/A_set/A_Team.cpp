33520f9b0bda3a38c8e925f67d73370d1063daa7
Shaurya_Mehta_0605 <shauryarajmehta0605@gmail.com>
2026-06-10 13:15:12 +0530

    Create A_Team.cpp

---
+++ Codeforce/A_set/A_Team.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int ans = 0;
+ 
+    while (n--) {
+        int p, v, t;
+        cin >> p >> v >> t;
+ 
+        if (p + v + t >= 2)
+            ans++;
+    }
+ 
+    cout << ans;
+ 
+    return 0;
+}
